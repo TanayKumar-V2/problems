2b780a2bdf6562a6fa29d8c168a4e9050fe2bb34
TanayKumar-V2 <tanayk726@gmail.com>
2025-08-10 21:05:49 +0530

    "added pattern code"

--- pattern_print.cpp
+++ pattern_print.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main(){
+
+    int n=3;
     
+    int num=1;
+
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<num<<' ';
+            num++;
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
